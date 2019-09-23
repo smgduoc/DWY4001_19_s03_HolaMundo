4cbe553b72f747868f1a860afa9e3f67f847ab62
Sebastian <s.mezag@alumnos.duoc.cl>
2019-09-23 12:47:54 -0300

    Agregado el ejemplo de Ciclos

---
+++ ejemplo_ciclos.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+#la primera linea es para linux, no es necesaria en Windows
+
+#ojo con la sintaxis, los ESPACIOS importan
+#no es necesario declarar las variables
+
+i = 6
+while i > 0:
+	print(i)
+	i = i - 1
+
+ciudades = ["Arica" , "Iquique" , "Antofagasta" , "Copiapo"]
+
+for ciudad in ciudades:
+	print(ciudad)
